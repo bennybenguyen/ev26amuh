d94e2b1d803036e882c013e94e0732947e6d9d79
Benny Nguyen <info@bennynguyen.de>
2021-11-12 15:44:50 +0100

    Structure of the repository

---
+++ ev26amuh/function.py
@@ -0,0 +1,7 @@
+import numpy as np
+from ipywidgets import interact, fixed
+from PIL import Image
+
+def imshow(X, resize=None):
+
+    pass
---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='ev26amuh',
+      version='0.1',
+      author='Benny Nguyen',
+      author_email='benny.nguyen'
