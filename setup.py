1c747baa682e424ce76a9fcb4bda10c642b636e2
Benny Nguyen <info@bennynguyen.de>
2021-11-12 15:56:34 +0100

    Structure of the repository

--- setup.py
+++ setup.py
@@ -4,4 +4,6 @@ from setuptools import find_packages
 setup(name='ev26amuh',
       version='0.1',
       author='Benny Nguyen',
-      author_email='benny.nguyen'
+      author_email='benny.nguyen',
+      packages=find_packages(),
+      install_requires=['numpy', 'Pillow', 'ipywidgets'])
