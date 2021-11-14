26cbce985157f1da90cab3d52ae2a9f956aa1314
Benny Nguyen <info@bennynguyen.de>
2021-11-14 17:48:55 +0100

    adding content to function.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 from setuptools import find_packages
 
-setup(name='ev26amuh',
+setup(name='idm-id',
       version='0.1',
       author='Benny Nguyen',
       author_email='benny.nguyen',
       packages=find_packages(),
-      install_requires=['numpy', 'Pillow', 'ipywidgets'])
+      install_requires=['numpy', 'Pillow', 'ipywidgets','matplotlib'])
